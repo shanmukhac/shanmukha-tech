6f190c8e0c6734cfa372db0141630d8779285706
opsbobby <shanmukhas9817@yahoo.com>
2026-07-23 18:15:35 +0530

    day2TerraformLearnings

---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  mime_types = {
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    svg  = "image/svg+xml"
+    ico  = "image/x-icon"
+  }
+}
