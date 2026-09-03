6fedc54beb40304d47e12fd78faa478e572e33e8
seanscans <sean.scales@gmail.com>
2026-09-04 00:15:12 +0530

    final-terraform-edit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
@@ -34,28 +34,41 @@ resource "aws_s3_bucket" "websiteHosting" {
   }
 }
 
-resource "aws_acm_certificate" "cert" {
-  provider = aws.virginia
-
-  domain_name       = "shanmukha.tech"
-
-  subject_alternative_names = [
-    "www.shanmukha.tech",
-    "shanmukhas.in",
-    "www.shanmukhas.in"
-  ]
-
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+data "aws_route53_zone" "tech" {
+  name         = "shanmukha.tech"
+  private_zone = false
 }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
+# resource "aws_acm_certificate" "cert" {
+#   provider = aws.virginia
+
+#   domain_name       = "shanmukha.tech"
+
+#   subject_alternative_names = [
+#     "shanmukhas.in"
+#   ]
+
+#   validation_method = "DNS"
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   provider = aws.virginia
+
+#   certificate_arn = aws_acm_certificate.cert.arn
+# }
+
+data "aws_acm_certificate" "existing_cert" {
   provider = aws.virginia
 
-  certificate_arn = aws_acm_certificate.cert.arn
+  domain = "shanmukha.tech"
+
+  statuses = ["ISSUED"]
+
+  most_recent = true
 }
 
 resource "aws_cloudfront_distribution" "websiteDistribution" {
@@ -72,10 +85,7 @@ resource "aws_cloudfront_distribution" "websiteDistribution" {
   default_root_object = "index.html"
 
   aliases = [
-    "shanmukha.tech",
-    "www.shanmukha.tech",
-    "shanmukhas.in",
-    "www.shanmukhas.in"
+    "shanmukha.tech"
   ]
 
   default_cache_behavior {
@@ -106,54 +116,78 @@ resource "aws_cloudfront_distribution" "websiteDistribution" {
   }
 
   viewer_certificate {
-  acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
-  ssl_support_method       = "sni-only"
-  minimum_protocol_version = "TLSv1.2_2021"
+    acm_certificate_arn      = data.aws_acm_certificate.existing_cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 }
+
+resource "aws_route53_record" "tech_ipv4" {
+  zone_id = data.aws_route53_zone.tech.zone_id
+  name    = "shanmukha.tech"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.websiteDistribution.domain_name
+    zone_id                = aws_cloudfront_distribution.websiteDistribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "tech_ipv6" {
+  zone_id = data.aws_route53_zone.tech.zone_id
+  name    = "shanmukha.tech"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.websiteDistribution.domain_name
+    zone_id                = aws_cloudfront_distribution.websiteDistribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_cloudfront_origin_access_control" "oac" {
-  name = "portfolio-website-shanmukha-tech-oac"
+  name                              = "portfolio-website-shanmukha-tech-oac"
   origin_access_control_origin_type = "s3"
-  signing_behavior = "always"
-  signing_protocol = "sigv4"
-  
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+
 }
 
 resource "aws_s3_object" "portfolioSite" {
 
-  for_each = fileset("${path.module}/shanmukha-tech","**/*")
-  bucket = aws_s3_bucket.websiteHosting.bucket
-  key    = each.value
-  source = "${path.module}/shanmukha-tech/${each.value}"
-  etag   = filemd5("${path.module}/shanmukha-tech/${each.value}")
+  for_each = fileset("${path.module}/shanmukha-tech", "**/*")
+  bucket   = aws_s3_bucket.websiteHosting.bucket
+  key      = each.value
+  source   = "${path.module}/shanmukha-tech/${each.value}"
+  etag     = filemd5("${path.module}/shanmukha-tech/${each.value}")
 
-  content_type = lookup(local.mime_types, reverse(split(".", each.value))[0],"application/octet-stream")
+  content_type = lookup(local.mime_types, reverse(split(".", each.value))[0], "application/octet-stream")
 }
 
 resource "aws_s3_bucket_policy" "s3_policy" {
   bucket = aws_s3_bucket.websiteHosting.id
   policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Statement1",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "cloudfront.amazonaws.com"
-      },
-      "Action": [
-        "s3:GetObject",
-        # "s3:ListBucket"
-      ],
-      "Resource": "${aws_s3_bucket.websiteHosting.arn}/*"
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Statement1",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "s3:GetObject",
+          # "s3:ListBucket"
+        ],
+        "Resource" : "${aws_s3_bucket.websiteHosting.arn}/*"
 
-      Condition = {
+        Condition = {
           StringEquals = {
             "AWS:SourceArn" = aws_cloudfront_distribution.websiteDistribution.arn
           }
         }
-    }
-  ]
-})
+      }
+    ]
+  })
 }
